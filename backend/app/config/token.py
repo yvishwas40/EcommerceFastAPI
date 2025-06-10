9961cc1e58357a0c5f70e233f48a7cbf9cadda98
yvishwas40 <yvishwas40@gmail.com>
2025-06-10 08:51:24 +0530

    all changed to .env

--- backend/app/config/token.py
+++ backend/app/config/token.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime, timedelta
 from jwt import PyJWTError
 import jwt
@@ -6,13 +7,15 @@ from config.database import get_db
 from users.usersservice import UserService
 from fastapi.security import OAuth2PasswordBearer
 from fastapi import Depends, HTTPException, status
+from dotenv import load_dotenv
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+load_dotenv()  # Load from .env
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
-
 
 def create_access_token(data: dict):
     to_encode = data.copy()
@@ -21,12 +24,10 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-
 def verify_token(token: str, credentials_exception, db: Session = Depends(get_db)):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         email: str = payload.get("sub")
-
         if email is None:
             raise credentials_exception
         token_data = email
@@ -34,12 +35,10 @@ def verify_token(token: str, credentials_exception, db: Session = Depends(get_db
         raise credentials_exception
 
     user = UserService.get_user(email=token_data, db=db)
-
     if not user:
         raise credentials_exception
 
     return user
-
 
 def get_currentUser(db: Session = Depends(get_db), data: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
@@ -47,6 +46,4 @@ def get_currentUser(db: Session = Depends(get_db), data: str = Depends(oauth2_sc
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-
     return verify_token(token=data, credentials_exception=credentials_exception, db=db)
-
